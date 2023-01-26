da737e327d028737aa0253bbbc290df51e8a3a93
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-01-26 19:20:12 +0530

    test: add tests for ship factory

---
+++ tests/shipFactory.test.js
@@ -0,0 +1,21 @@
+import shipFactory from '../js_modules/shipFactory';
+
+describe('Ship Factory', () => {
+  it('should return the length of the ship', () => {
+    const ship = shipFactory(4);
+    expect(ship.length).toBe(4);
+  });
+
+  it('should return true when the ship is sunk', () => {
+    const ship = shipFactory(2);
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+
+  it('should return false when the ship is not sunk', () => {
+    const ship = shipFactory(3);
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+  });
+});
